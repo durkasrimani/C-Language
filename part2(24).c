3669e3a8b8ddb5199d6c0603e8e1c4fb99b3e20c
durkasrimani <durkasrimani937@gmail.com>
2026-02-07 11:54:49 +0530

    Refactor main function for clarity and formatting

--- part2(24).c
+++ part2(24).c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
-    int a,Positive,Negative,Zero;
+    int a;
     scanf("%d",&a);
-    (a>0)? printf("Positive"):(a<0)? printf("Negative"):printf ("Zero");
+    (a>0)?printf("positive"):(a<0)?printf("negative"):printf("zero");
     return 0;
 }
