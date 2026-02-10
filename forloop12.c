3d3535d1d17d38e03e9dec54d5b584b5913ac3c1
durkasrimani <durkasrimani937@gmail.com>
2026-02-10 18:37:56 +0530

    Add files via upload

---
+++ forloop12.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,p,sum=0,i;
+    scanf("%d %d",&n,&p);
+    for(i=1;i<=1;i++){
+        sum=sum+(pow(n,p));
+    }
+    printf("%d",sum);
+    return 0;
+
+}
