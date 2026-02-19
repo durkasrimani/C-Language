f61024b0f59e265d67ad4ba777fe84de5c6c905d
durkasrimani <durkasrimani937@gmail.com>
2026-02-19 19:38:24 +0530

    Add files via upload

---
+++ nestedloop17.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+     for(i=0;i<=n;i++){
+        for(j=0;j<=(n*2+1);j++){
+            if((n-i)<j && j<n+i+1){
+                printf("  ");
+            }
+            else{
+                printf("* ");
+            }
+        }
+         printf("\n");
+    }
+    for(i=0;i<=n;i++){
+        for(j=0;j<=(n*2+1);j++){
+            if(i<j && j<((n*2)+1-i)){
+                printf("  ");
+            }
+            else{
+            printf("* ");}
+        }
+        printf("\n");
+    }
+   
+    return 0;
+}
