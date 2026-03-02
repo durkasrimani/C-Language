e5ef6d1128bf31e53da731520c7745a25fdafd39
durkasrimani <durkasrimani937@gmail.com>
2026-03-02 15:12:06 +0530

    Add files via upload

---
+++ f20.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int calculator(int a,int b,int c){
+    for(int i=0;i<)
+}
+int main(){
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
+    int d=calculator(a,b,c);
+    printf("%d",d);
+    return 0;
+}
